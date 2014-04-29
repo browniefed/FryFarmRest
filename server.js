571f8d0291f6d768bf438029416e0dd1784db712
Jason Brown <iam@browniefed.com>
2014-04-28 20:48:39 -0700

    Just throwing up a blank /

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ server.use(restify.CORS());
 server.use(restify.queryParser());
 server.use(restify.gzipResponse());
 
+
+server.post('/', function(req, res, next) {
+	res.send('This is the API for FryFarms')
+});
+
+
+
 server.listen(process.env.PORT || 8080, function() {});
